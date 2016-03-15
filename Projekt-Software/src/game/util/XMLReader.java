2bdfa029f8531b9280a7f4eec173dfb781d57068
Mathias R. Larsen <Mathias R. Larsen@MathiasRLarsen.bsk>
2016-03-15 18:30:43 +0100

    game.util pakke færdig

---
+++ Projekt-Software/src/game/util/XMLReader.java
@@ -0,0 +1,37 @@
+package game.util;
+
+import java.io.File;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class XMLReader {
+
+	private Document document;
+
+	public XMLReader(String filePath){
+		loadDocument(filePath);
+	}
+
+	// References a file. and parsing it to the documentproperty
+	public void loadDocument(String filePath){
+		try{
+			File file =  new File(filePath);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			document = dBuilder.parse(file);
+			document.getDocumentElement().normalize(); 
+		}catch(Exception e){
+
+		}
+	}
+
+	// Gets stringNumber element text in elements named stringname
+	public String getElement(String stringName, int stringNumber){
+		NodeList nList = document.getElementsByTagName(stringName);
+		return nList.item(stringNumber).getTextContent();
+	}
+}
