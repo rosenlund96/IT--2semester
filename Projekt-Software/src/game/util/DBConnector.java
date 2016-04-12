8808bc7e9e80150faff56ccdc926e0d85e7a2739
adamleth <adamleth@gmail.com>
2016-04-12 11:02:51 +0200

    Færdig database connector

--- Projekt-Software/src/game/util/DBConnector.java
+++ Projekt-Software/src/game/util/DBConnector.java
@@ -12,15 +12,36 @@ public class DBConnector {
 	public class Connector {
 		private final String HOST     = "Localhost";
 		private final int    PORT     = 3306;
-		private final String DATABASE = "ct";
+		private final String DATABASE = "Projekt";
 		private final String USERNAME = "root"; 
 		private final String PASSWORD = "";
 		private Connection connection;
-	}
-	
-	public DBConnector() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver")
+
+
+		public Connector() {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+				connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+
+		public Connection getConnection() {
+			return connection;
+		}
+
+		public ResultSet doQuery(String query) throws SQLException{
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			return rs;
+		}
+		
+		public void doUpdate(String query) throws SQLException{
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(query);
 		}
 	}
 }
