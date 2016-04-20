377e17ef13805ebb4a6ab5aa55f301edd48a6975
Mathias R. Larsen <Mathias R. Larsen@DESKTOP-9TPNAH0.bsk>
2016-04-20 22:53:10 +0200

    Commit gik i lort undervejs...

--- Projekt-Software/src/game/entities/cards/Refuge.java
+++ Projekt-Software/src/game/entities/cards/Refuge.java
@@ -1,11 +1,5 @@
 package game.entities.cards;
 
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
-
 import game.boundaries.Outputable;
 import game.controllers.GameController;
 import game.entities.FieldManager;
