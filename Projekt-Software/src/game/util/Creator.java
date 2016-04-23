de100536d82565ea26067695608d32a16663bd62
adamleth <adamleth@gmail.com>
2016-04-23 15:02:19 +0200

    små ændringer bl.a. foreign keys

--- Projekt-Software/src/game/util/Creator.java
+++ Projekt-Software/src/game/util/Creator.java
@@ -9,7 +9,7 @@ public class Creator {
 	DBConnector con = new DBConnector();
 	
 	
-	//Opretter en ny database og returner navnet på denne
+	//Opretter en ny database og returner navnet pï¿½ denne
 	public LocalDateTime createGameDB(LocalDateTime time){
 		try {
 			con.doQuery(createDB);
@@ -18,19 +18,20 @@ public class Creator {
 		}
 		return time;
 	}
-	//Opretter de fornødne tabeller i databasen
+	//Opretter de fornï¿½dne tabeller i databasen
 	public void createTables(){
 		createGame();
-		createOwnable();
-		createBoard();
+		createPlayersList();
+		createField();
+
 	}
 	public void createGame(){
 		//Gemmer alt fra GameController.
 		String query = ("create table Game" + 
-						"game_id time, " + 
-						"game_State varchar(10)" +
-						"turnNumber int" +
-						"primary key (time), " + 
+						"(game_id int (11) NOT NULL, " + 
+						"game_State varchar(45) NOT NULL, " +
+						"turnNumber int (11) NOT NULL, " +
+						"primary key (game_id), " + 
 						"foreign key (player_id) references player_LIST (player_id));");
 		try {
 			con.doQuery(query);
@@ -41,14 +42,15 @@ public class Creator {
 	}
 	public void createPlayersList(){
 		String query = ("create table player_List" +
-						"player_id int NOT NULL, " +
-						"playerName varchar(40) NOT NULL," + 
-						"playerBalance int NOT NULL," +
-						"playerPosition int NOT NULL," +
-						"housesOwned int, NOT NULL" +
-						"hotelsOwned int, NOT NULL" + 
-						"prisonCards_owned int, NOT NULL" +
-						"primary key (playerName,playerBalance,playerPosition)");
+						"(player_id int (11) NOT NULL, " +
+						"playerName varchar(45) NOT NULL, " + 
+						"playerBalance int (11) DEFAULT NULL, " +
+						"housesOwned int (2) DEFAULT NULL, " +
+						"hotelsOwned int (2) DEFAULT NULL, " + 
+						"prisonCards int (1) DEFAULT NULL, " +
+						"primary key (player_id), " + 
+						"foreign key (housesOwned) references Field (houseOnField), " +
+						"foreign key (hotelsOwned) references Field (hotelOnField));");
 		
 		try {
 			con.doQuery(query);
@@ -57,11 +59,15 @@ public class Creator {
 			e.printStackTrace();
 		}
 	}
-	public void createOwnable(){
+	public void createField(){
 		//Gemmer alt om felter.
-		String query = ("create table Ownable" +
-						"(ownable boolean, " +
-						"");
+		String query = ("create table Field" +
+						"(fieldNo int (11) NOT NULL, " +
+						"fieldOwner varchar (45) DEFAULT NULL, " +
+						"houseOnField int(1) DEFAULT NULL, " + 
+						"hotelOnField tinyint(1) DEFAULT NULL, " +
+						"primary key (fieldNo)" + 
+						"foreign key (fieldOwner) references player_List (player_id));");
 		try {
 			con.doQuery(query);
 		} catch (SQLException e) {
@@ -69,16 +75,5 @@ public class Creator {
 			e.printStackTrace();
 		}
 	}
-	public void createBoard() {
-		//Gemmer alt på boardet, herunder positioner og bygninger
-		String query = ("create table Board" +
-						"field_NoOwnedBy int,"+
-						"housesOnFields int," +
-						"");
-		try {
-			con.doQuery(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+	
 }
