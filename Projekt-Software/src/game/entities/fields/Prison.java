dce5ad10b01e95838b6de6be426eb726fd2dc32f
adamleth <adamleth@gmail.com>
2016-04-24 13:33:58 +0200

    små ændringer

--- Projekt-Software/src/game/entities/fields/Prison.java
+++ Projekt-Software/src/game/entities/fields/Prison.java
@@ -8,7 +8,7 @@ import game.entities.Player;
 import game.util.DieCup;
 
 public class Prison extends AbstractField{
-	
+
 	private int fine;
 	private DieCup dices;
 	private int fieldNo;
@@ -19,7 +19,7 @@ public class Prison extends AbstractField{
 		dices = new DieCup();
 		fine = 1000; 
 		this.fieldNo = fieldNo;
-		
+
 	}
 
 	public void setImprisoned(boolean isImprisoned, Player player){
@@ -47,21 +47,21 @@ public class Prison extends AbstractField{
 			payFine(player);
 			setTimeInPrison(0, player);
 			setImprisoned(false, player);
-		break;
+			break;
 		case 2:
 			output.showRollingDiceForRent(player.getName());
 			output.setDice(dices.getDice());
-			
+
 			if (dices.getDie1()==dices.getDie2()) {
 				setImprisoned(false, player);
 				setTimeInPrison(0, player);
-				
-				
+
+
 			}
 			else setTimeInPrison(player.gettimeInPrison()+1, player);
-			    
-			    
-		break; 
+
+
+			break; 
 		case 3:
 			if (player.getoutOfJailCard()==0)	{
 				output.showNoCardMessage(player.getName());
@@ -72,26 +72,26 @@ public class Prison extends AbstractField{
 				setTimeInPrison(0, player);
 				setOutOfJailCard(player.getoutOfJailCard()-1, player);
 			}
-		break;
+			break;
 		case 4:
 			setTimeInPrison(player.gettimeInPrison()+1, player);
 			output.showDoTimeMessage(player);
 			break;
+		}
 	}
-	}
-	
-	
+
+
 	@Override
 	public void landOnField(Player player) {
 		if(fieldNo==11){
-		player.setTimeInPrison(player.gettimeInPrison()+1);
-		setImprisoned(true, player);
-		initializeChoice(player);
+			player.setTimeInPrison(player.gettimeInPrison()+1);
+			setImprisoned(true, player);
+			initializeChoice(player);
 		}
-	else {
-		setImprisoned(false, player);
-		setTimeInPrison(0, player);
-		
-	}
+		else {
+			setImprisoned(false, player);
+			setTimeInPrison(0, player);
+
+		}
 	}	
-	}
+}
